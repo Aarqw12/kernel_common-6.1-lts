45fa9b3554fb65242a9a968e9e4ee9551186a6fc
Edmond Chung <edmondchung@google.com>
2024-09-13 22:19:49 +0000

    Revert "IOCTL: checkpatch fixes"
    This reverts commit 3e466b6a58cb0859f39581864900d0e47e6e2989.
    
    Reason for revert: b/366547001, broke camera functionalities
    
    Bug: 366547001
    Change-Id: Id88caecb33b2a6349bd169668f251ee5ff992c94

--- lwis_ioctl_past.h
+++ lwis_ioctl_past.h
@@ -1,14 +1,3 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Google LWIS IOCTL Handler
- *
- * Copyright (c) 2024 Google, LLC
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- */
-
 #include "lwis_ioctl.h"
 
 extern struct cmd_transaction_submit_ops transaction_cmd_v5_ops;
