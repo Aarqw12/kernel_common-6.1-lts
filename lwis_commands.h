aca9e3f40ac3294b6b94d9f60045dee1d3ec1a4a
Edmond Chung <edmondchung@google.com>
2024-09-16 09:43:59 -0700

    Remove boilerplate and change to SPDX
    Bug: 356643083
    Test: Build
    Change-Id: I4dae5b985eec3398d983427ec63fc6de6cc6adc0
    Signed-off-by: Edmond Chung <edmondchung@google.com>

--- lwis_commands.h
+++ lwis_commands.h
@@ -4,10 +4,6 @@
  * Google LWIS IOCTL Commands and Data Structures
  *
  * Copyright (c) 2018 Google, LLC
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 /* END-INTERNAL */
 
--- lwis_fence.c
+++ lwis_fence.c
@@ -3,10 +3,6 @@
  * Google LWIS Fence
  *
  * Copyright (c) 2022 Google, LLC
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <linux/dev_printk.h>
