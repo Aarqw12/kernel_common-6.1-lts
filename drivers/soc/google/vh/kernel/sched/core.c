0793fc312d4e9a353c8326ba62bed9a85e4929c7
Qais Yousef <qyousef@google.com>
2024-12-03 13:27:37 +0000

    vh: sched: Always protect raw_spin_locks with _irqsave variant [ DO NOT MERGE ANYWHERE ]
    We can't rely on the caller to have disabled the interrupts. Many of
    scheduler operations can happen from interrupt context e.g:
    load_balance() or an IPI. Instead of relying on carefully auditing the
    code to know we need and irqsave variant or not, enforce it by default.
    
    Since flags name clashes with arg name in some function, use irqflags
    everywhere to pass to the _irqsave functions.
    
    Bug: 382033293
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: I4aa5018495ee5e8ab98682d0377da8f5ce19414a

--- drivers/soc/google/vh/kernel/sched/core.c
+++ drivers/soc/google/vh/kernel/sched/core.c
@@ -197,6 +197,7 @@ void vh_sched_switch_pixel_mod(void *data, bool preempt, struct task_struct *pre
 void rvh_enqueue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags)
 {
 	struct vendor_task_struct *vp = get_vendor_task_struct(p);
+	unsigned long irqflags;
 	int group;
 
 	if (!static_branch_unlikely(&enqueue_dequeue_ready))
@@ -209,13 +210,13 @@ void rvh_enqueue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p
 		}
 	}
 
-	raw_spin_lock(&vp->lock);
+	raw_spin_lock_irqsave(&vp->lock, irqflags);
 	if (vp->queued_to_list == LIST_NOT_QUEUED) {
 		group = get_vendor_group(p);
 		add_to_vendor_group_list(&vp->node, group);
 		vp->queued_to_list = LIST_QUEUED;
 	}
-	raw_spin_unlock(&vp->lock);
+	raw_spin_unlock_irqrestore(&vp->lock, irqflags);
 
 	/*
 	 * uclamp filter for RT tasks. CFS tasks are handled in
@@ -229,6 +230,7 @@ void rvh_enqueue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p
 void rvh_dequeue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags)
 {
 	struct vendor_task_struct *vp = get_vendor_task_struct(p);
+	unsigned long irqflags;
 	int group;
 
 	if (!static_branch_unlikely(&enqueue_dequeue_ready))
@@ -239,13 +241,13 @@ void rvh_dequeue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p
 		update_uclamp_stats(rq->cpu, rq_clock(rq));
 #endif
 
-	raw_spin_lock(&vp->lock);
+	raw_spin_lock_irqsave(&vp->lock, irqflags);
 	if (vp->queued_to_list == LIST_QUEUED) {
 		group = get_vendor_group(p);
 		remove_from_vendor_group_list(&vp->node, group);
 		vp->queued_to_list = LIST_NOT_QUEUED;
 	}
-	raw_spin_unlock(&vp->lock);
+	raw_spin_unlock_irqrestore(&vp->lock, irqflags);
 
 	/*
 	 * Reset uclamp filter flags unconditionally for both RT and CFS.
@@ -284,8 +286,9 @@ static void set_adpf_inheritance(struct task_struct *p, unsigned int type, int v
 {
 	struct vendor_task_struct *vp = get_vendor_task_struct(p);
 	struct vendor_inheritance_struct *vi = get_vendor_inheritance_struct(p);
+	unsigned long irqflags;
 
-	raw_spin_lock(&vp->lock);
+	raw_spin_lock_irqsave(&vp->lock, irqflags);
 
 	if (task_on_rq_queued(p)) {
 		bool old_adpf = get_adpf(p, true);
@@ -299,7 +302,7 @@ static void set_adpf_inheritance(struct task_struct *p, unsigned int type, int v
 		vi_set_adpf(vi, type, val);
 	}
 
-	raw_spin_unlock(&vp->lock);
+	raw_spin_unlock_irqrestore(&vp->lock, irqflags);
 }
 
 static void set_performance_inheritance_locked(struct task_struct *p, struct task_struct *pi_task,
@@ -373,11 +376,11 @@ static void set_performance_inheritance_locked(struct task_struct *p, struct tas
 static inline void set_performance_inheritance(struct task_struct *p, struct task_struct *pi_task,
 	unsigned int type)
 {
-	unsigned long flags;
+	unsigned long irqflags;
 
-	raw_spin_lock_irqsave(&p->pi_lock, flags);
+	raw_spin_lock_irqsave(&p->pi_lock, irqflags);
 	set_performance_inheritance_locked(p, pi_task, type);
-	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
+	raw_spin_unlock_irqrestore(&p->pi_lock, irqflags);
 }
 
 void vh_binder_set_priority_pixel_mod(void *data, struct binder_transaction *t,
